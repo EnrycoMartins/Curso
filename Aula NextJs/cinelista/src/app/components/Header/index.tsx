d2427031050f2bbd0ca7cec3af9e638987aa43f5
Enryco Martins <enrycomartins@gmail.com>
2026-09-01 16:35:43 -0300

    Inicio da aula sobre next.js

---
+++ Aula NextJs/cinelista/src/app/components/Header/index.tsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+
+
+    return (
+        <header>
+            <h1>Cinelista</h1>
+            <nav>
+                <a href="">Inicío</a>
+                <a href="">Em Alta</a>
+                <a href="">Populares</a>
+                <a href="">Top Filmes</a>
+            </nav>
+        </header>
+    );
+}
+export default Header;
